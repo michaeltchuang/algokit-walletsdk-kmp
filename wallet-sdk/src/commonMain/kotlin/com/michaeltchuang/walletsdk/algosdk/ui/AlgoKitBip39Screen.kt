44143d8d69fa3401f262f7efbc5803b4407a0f13
“Android <mithilesh13cs22@gmail.com>
2025-06-21 00:42:37 +0530

    BottomSheet moved to wallet-sdk

--- wallet-sdk/src/commonMain/kotlin/com/michaeltchuang/walletsdk/algosdk/ui/AlgoKitBip39Screen.kt
+++ wallet-sdk/src/commonMain/kotlin/com/michaeltchuang/walletsdk/algosdk/ui/AlgoKitBip39Screen.kt
@@ -1,28 +1,74 @@
 package com.michaeltchuang.walletsdk.algosdk.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.michaeltchuang.walletsdk.algosdk.getEntropyFromMnemonic
+import com.michaeltchuang.walletsdk.ui.theme.AlgoKitTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+private const val SAMPLE_STRING =
+    "borrow among leopard smooth trade cake profit proud matrix bottom goose charge oxygen shine punch hotel era monitor fossil violin tip notice any visit"
 
 @Composable
-fun AlgoKitBip39Screen(text: String) {
-    Box(
-        Modifier.fillMaxHeight(0.5f).fillMaxWidth(),
-        contentAlignment = Alignment.Center
+fun AlgoKitBip39Screen(even: (event: AlgoKitEvent) -> Unit) {
+    Column(
+        Modifier.fillMaxHeight(0.5f).fillMaxWidth()
+            .background(color = AlgoKitTheme.colors.background),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        var entropy by remember { mutableStateOf("") }
+
+        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd) {
+            IconButton(onClick = { even(AlgoKitEvent.ClOSE) }) {
+                Icon(
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Icon Button",
+                    tint = AlgoKitTheme.colors.textMain,
+                    modifier = Modifier.size(48.dp).padding(8.dp)
+                )
+            }
+        }
+        Button(onClick = {
+            entropy = getEntropyFromMnemonic(SAMPLE_STRING)
+            even(AlgoKitEvent.ACCOUNT_CREATED)
+        }) {
+            Text("Account Create")
+        }
+        Spacer(modifier = Modifier.size(16.dp))
         Text(
             modifier = Modifier.wrapContentSize(),
-            text = getEntropyFromMnemonic(text),
-            color = Color.Black,
+            text = entropy,
+            color = AlgoKitTheme.colors.textMain ,
             fontSize = 18.sp
         )
     }
+}
+
+
+@Preview
+@Composable
+fun AlgoKitBip39ScreenPreview() {
+    AlgoKitBip39Screen() {}
 }
