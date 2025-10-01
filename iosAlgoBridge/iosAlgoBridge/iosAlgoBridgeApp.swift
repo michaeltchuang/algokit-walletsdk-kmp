76af824745525e3eb46ab643608d26b9a21bf144
Michael T Chuang <github@michaeltchuang.com>
2025-09-30 17:26:09 -0700

    feat: add in working ios sample app

---
+++ iosAlgoBridge/iosAlgoBridge/iosAlgoBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAlgoBridgeApp.swift
+//  iosAlgoBridge
+//
+//  Created by Michael Chuang on 9/15/25.
+//
+
+import SwiftUI
+
+@main
+struct iosAlgoBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
