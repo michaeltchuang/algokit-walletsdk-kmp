c073413757eb5cf8c5e268b85dbde846cefa7e9c
Michael T Chuang <github@michaeltchuang.com>
2025-10-09 17:37:58 -0700

    fix: create falcon account on onboarding type screen for android

--- wallet-sdk/src/commonMain/kotlin/com/michaeltchuang/walletsdk/account/domain/usecase/core/AccountAdditionUseCase.kt
+++ wallet-sdk/src/commonMain/kotlin/com/michaeltchuang/walletsdk/account/domain/usecase/core/AccountAdditionUseCase.kt
@@ -76,8 +76,6 @@ class AccountAdditionUseCase(
                     createAccount.orderIndex,
                 )
             }
-            // }
-            // }
         }
     }
 
--- wallet-sdk/src/commonMain/kotlin/com/michaeltchuang/walletsdk/account/presentation/viewmodels/OnboardingAccountTypeViewModel.kt
+++ wallet-sdk/src/commonMain/kotlin/com/michaeltchuang/walletsdk/account/presentation/viewmodels/OnboardingAccountTypeViewModel.kt
@@ -2,6 +2,7 @@ package com.michaeltchuang.walletsdk.account.presentation.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.michaeltchuang.walletsdk.account.data.mapper.entity.AccountCreationFalcon24TypeMapper
 import com.michaeltchuang.walletsdk.account.data.mapper.entity.AccountCreationHdKeyTypeMapper
 import com.michaeltchuang.walletsdk.account.domain.model.core.AccountCreation
 import com.michaeltchuang.walletsdk.account.domain.repository.local.HdSeedRepository
@@ -20,7 +21,7 @@ class OnboardingAccountTypeViewModel(
   /*  private val androidEncryptionManager: AndroidEncryptionManager,
     private val aesPlatformManager: AESPlatformManager,
     private val runtimeAwareSdk: RuntimeAwareSdk, */
-    private val accountCreationHdKeyTypeMapper: AccountCreationHdKeyTypeMapper,
+  private val accountCreationFalcon24TypeMapper: AccountCreationFalcon24TypeMapper,
     private val hdSeedRepository: HdSeedRepository,
     private val stateDelegate: StateDelegate<ViewState>,
     private val eventDelegate: EventDelegate<ViewEvent>,
@@ -43,52 +44,32 @@ class OnboardingAccountTypeViewModel(
         }
     }
 
-    fun createHdKeyAccount() {
+    fun createFalcon24Account() {
         viewModelScope.launch {
             val wallet = createBip39Wallet()
-            val hdKeyAddress = wallet.generateAddress(HdKeyAddressIndex(0, 0, 0))
-            val hdKeyType =
-                accountCreationHdKeyTypeMapper(
+            val mnemonic = wallet.getMnemonic().words.joinToString(" ")
+            val falcon24 = wallet.generateFalcon24Address(mnemonic)
+            val falcon24Type =
+                accountCreationFalcon24TypeMapper(
                     wallet.getEntropy().value,
-                    hdKeyAddress,
+                    falcon24,
                     seedId = null,
                 )
             val accountCreation =
                 AccountCreation(
-                    address = hdKeyAddress.address,
+                    address = falcon24.address,
                     customName = null,
                     isBackedUp = false,
-                    type = hdKeyType,
+                    type = falcon24Type,
                     creationType = CreationType.CREATE,
                 )
-            // Store the account creation data in the manager
-            AccountCreationManager.storePendingAccountCreation(accountCreation)
-            eventDelegate.sendEvent(ViewEvent.AccountCreated(accountCreation))
-        }
-    }
 
-    fun createAlgoAccount() {
-        viewModelScope.launch {
-            try {
-                createAlgo25Account()?.let {
-                    val secretKey = it.secretKey
-                    val accountCreation =
-                        AccountCreation(
-                            address = it.address,
-                            customName = null,
-                            isBackedUp = false,
-                            type = AccountCreation.Type.Algo25(secretKey),
-                            creationType = CreationType.CREATE,
-                        )
-                    // Store the account creation data in the manager
-                    AccountCreationManager.storePendingAccountCreation(accountCreation = accountCreation)
-                    eventDelegate.sendEvent(ViewEvent.AccountCreated(accountCreation = accountCreation))
-                } ?: run {
-                    displayError("Failed to create account")
-                }
-            } catch (e: Exception) {
-                displayError(e.message ?: "Unknown error")
-            }
+            AccountCreationManager.storePendingAccountCreation(accountCreation)
+            eventDelegate.sendEvent(
+                ViewEvent.AccountCreated(
+                    accountCreation,
+                ),
+            )
         }
     }
 
